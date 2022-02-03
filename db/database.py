ceb1c6cd58a665cb35d4c3be22cf6cc871e0a81e
Omar Walid <omarwalidhamed@gmail.com>
2022-02-03 13:15:36 +0200

    main database

---
+++ db/database.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+from .users import users_dao
+from .messages import messages_dao
+from .media import media_dao
+from .requests import requests_dao
+
+class app_database:
+    instance = None
+    def __init__(self, host = 'localhost', port = 27017):
+        self.client = MongoClient(host = host, port = port)
+        self.db = self.client['chat']
+        
+        
+    def get_users_dao(self) -> users_dao:
+        return users_dao(self.db)
+
+    def get_media_dao(self) -> media_dao:
+        return media_dao(self.db)
+
+    def get_messages_dao(self) -> messages_dao:
+        return messages_dao(self.db)
+
+    def get_requests_dao(self) -> requests_dao:
+        return requests_dao(self.db)
+    
+    def get_instance():
+        if app_database.instance == None:
+            app_database.instance = app_database()
+        return app_database.instance
