f047d8fbb006b266cd9373a30234aba549f7574c
Omar Walid <omarwalidhamed@gmail.com>
2022-04-23 13:05:53 +0200

    added global constants

---
+++ utils/constants.py
@@ -0,0 +1,22 @@
+# message syntax constants
+
+# Header fields
+HEADER_REQUEST = 'request'
+HEADER_FILE_TYPE = 'file_type'
+HEADER_TOKEN = 'token'
+
+# Request field values
+REQUEST_MESSAGE_SEND = 'send_message'
+REQUEST_MESSAGE_RECEIVE = 'receive_message'
+REQUEST_MESSAGE_DELETE = 'delete_message'
+REQUEST_MESSAGES_READ = 'messages_read'
+REQUEST_UPLOAD_FILE = 'upload_file'
+REQUEST_DOWNLOAD_FILE = 'download_file'
+REQUEST_POLL_MESSAGES = 'poll_messages'
+REQUEST_PENDING_MESSAGES = 'pending_messages'
+
+# Body fields
+BODY_TO_ID = 'to_id'
+BODY_MESSAGE_ID = 'message_id'
+BODY_MESSAGE_TEXT = 'message_text'
+BODY_MEDIA_ID = 'media_id'
